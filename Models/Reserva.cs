cd8031f6c1cc7304d38de54b95f88a8336862646
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-12 00:20:31 -0300

    Implementacao das regras solicitadas no projeto final.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,17 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *IMPLEMENTADO*
+            
+            if ( Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTADO*
+                throw new Exception("Número de hóspedes é maior que a capacidade");
             }
         }
 
@@ -36,22 +38,23 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // *IMPLEMENTADO*
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // *IMPLEMENTADO*
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *IMPLEMENTADO*
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = Convert.ToDecimal(valor * 10 / 100);
+                return valor - desconto;
             }
 
             return valor;
